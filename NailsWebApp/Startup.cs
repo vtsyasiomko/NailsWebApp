735f187680965247f5153f31088899aaf6642606
Vladyslav Tsiasiomko <vtsyasiomko@outlook.com>
2015-10-07 15:51:47 +0300

    add Solution to repository

---
+++ NailsWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NailsWebApp.Startup))]
+namespace NailsWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
